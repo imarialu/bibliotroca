c10fb5f35b823056c801b3586a5799af0a526a5f
imarialu <marialuizaprado13@gmail.com>
2025-10-20 15:43:18 -0300

    feat: criando o card dos livros

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,28 @@
+import { DetailsButton } from "./DetailsButton"
+
+export function BookCard({imgBook, status, title, author, imgUser, nameUser}){
+    return(
+        <div className="flex w-[350px] h-[180px] p-3 gap-2 border-1 rounded-md border-[var(--border-purple)] transition duration-500 ease hover:shadow-[0px_0px_5px_2px_rgba(198,_174,_230,_0.4)]">
+            <div className="w-[120px] rounded-md bg-[var(--color-gray)]">
+                <img src={imgBook} alt="" className="w-full"/>
+            </div>
+
+            <div className="w-[250px]">
+                <p className="text-sm font-semibold">{status}</p>
+                <h2 className="text-base font-medium">{title.length > 24 ? `${title.substring(0, 25)}...` : title}</h2>
+                <p className="text-sm">{author}</p>
+
+                <DetailsButton/>
+                <hr className="text-[var(--border-purple)]"/>
+                <div className="flex gap-2 items-center mt-2">
+                    <div className="rounded-full bg-[var(--color-gray)] w-[25px] h-[25px]">
+                        <img src={imgUser} alt="" />
+                    </div>
+                    
+                    <p className="text-sm">{nameUser}</p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
---
+++ src/components/DetailsButton.jsx
@@ -0,0 +1,7 @@
+export function DetailsButton(){
+    return(
+        <button className="mt-4 mb-2 px-6 py-[0.2rem] rounded-2xl bg-[var(--border-purple)] text-sm text-[var(--color-purple)] font-semibold cursor-pointer transition duration-400 ease hover:bg-[var(--border-purple-hover)] ">
+            Detalhes
+        </button>
+    )
+}
