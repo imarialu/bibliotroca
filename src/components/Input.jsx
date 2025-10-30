1f8539d0f6cb3096163f36ae5c3e6969faaf1b55
imarialu <marialuizaprado13@gmail.com>
2025-10-30 16:35:21 -0300

    style: retira largura fixa do componente Input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 export function Input({label, type, placeholder, ...props}){
     return(
-        <div className="flex flex-col w-[300px] mb-2">
+        <div className="flex flex-col mb-2">
             <label className="mb-1 text-lg">{label}</label>
             <input type={type} placeholder={placeholder} {...props} className="border border-[var(--color-gray)] rounded-sm h-8 pl-2 mb-1 focus:outline-none focus:border-[var(--color-purple)]"/>
         </div>
